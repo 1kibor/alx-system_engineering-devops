05fd8b9938fad65cd8049c0c7085e1105dd071c5
1kibor <kibor259@gmail.com>
2024-05-15 01:50:52 -0700

    Repetition Token #0

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-# script that accepts one argument and passes it to a regular expression,
-# matching method
-# The regular expression should match the given cases
-
 puts ARGV[0].scan(/hbt{2,5}n/).join
